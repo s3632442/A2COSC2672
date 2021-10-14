7fcf0152c3c6cbbf38880433fdb4b97f36906cee
s3632442 <s3632442@student.rmit.edu.au>
2021-10-15 06:04:24 +1100

    multi

---
+++ go_opt_multi.sh
@@ -0,0 +1,21 @@
+while getopts ":abc:d:" opt; do
+case $opt in
+a)
+echo "-a was triggered!" >&2
+;;
+b)
+echo "-b was triggered!" >&2
+;;
+c)
+echo "-c was triggered!" >&2
+echo $OPTARG
+;;
+d)
+echo "-d was triggered!" >&2
+echo $OPTARG
+;;
+\?)
+echo "Invalid option: -$OPTARG" >&2
+;;
+esac
+done
